2a8eb1aa82a3fc463a5c95caf9341aaa12908d1d
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-22 11:38:52 +0200

    Create the page to add a category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,27 @@
 class GroupsController < ApplicationController
     def index
+        @user = current_user
+    end
+
+    def new
+        @user = current_user
+    end
+
+    def create
+        @user = current_user
+        @group = Group.new(group_params)
+        @group.user_id = @user.id
+        if @group.save
+            redirect_to user_groups_path(@user.id)
+        else
+            print 'Something went wrong'
+            render :new
+        end
+    end
+
+    private
+
+    def group_params
+      params.require(:group).permit(:name, :icon)
     end
 end
